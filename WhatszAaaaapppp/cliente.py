818670b194dad12eaeba92c3bd6c5724a0de1074
Lucas <lucas.ferreira26102001@gmail.com>
2024-04-26 06:58:38 -0300

    Trabalho de Aps feito em grupo

--- WhatszAaaaapppp/cliente.py
+++ WhatszAaaaapppp/cliente.py
@@ -1,77 +1,117 @@
-import sys
-import socket
+from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
+import tkinter
 
-SERVER_IP = ''
-SERVER_PORT = 8001
-
-
-def conexao():
-    ip = input("Entre com o endereço de ip desejado para começar a se comunicar")
-    conf = input(f"\no ip: {ip} esta correto?\n1-Sim/0-Não")
-    if conf == 0:
-        print("Desligando o programa...")
-        sys.exit()
-    return iniciar_conexao(ip)
+ip = None
+msg = None
 
 
-def verifica_ip(endereco_ip):
-    if len(endereco_ip) == 9 and endereco_ip is not None:
-        return True
-    print("Ip imcompativel, encerrando programa")
-    sys.exit()
+def chat():
+    mensagem.set(e_mensagem.get())
+    msg = mensagem.get()
+    if msg is not None:
+        if msg.lower() == 'exit':
+            fecha()
+            return
+        con_tcp.send(bytes(msg, "utf8"))
+        e_mensagem.delete(0, tkinter.END)
 
 
-def iniciar_conexao(ip):
-    print("Tentando conectar ao servidor")
-    verifica_ip(ip)
-    con_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    try:
-        ADDR = (ip, SERVER_PORT)
-        con_tcp.connect(ADDR)
-    except ConnectionError as error:
-        print("Conexao negada. Tente novamente!\n"
-              f"tipo de erro: `{type(error)}")
-        sys.exit()
-
-    return con_tcp
-
-
-def encerrar_conexao(con_tcp):
-    print("Encerrando conexão TCP...")
-    con_tcp.close()
-
-
-def chat(con_tcp):
-    print("Chat iniciado! Para sair escreva: exit ")
-
+def chatrecebe():
     while True:
-        mensagem = input("voce: ")
-
-        if mensagem is not None:
-            con_tcp.send(bytes(mensagem, "utf8"))
-            if mensagem == 'exit':
-                break
-
-        recv_mens = con_tcp.recv(1024).decode("utf8")
-
-        if recv_mens is not None:
-            print(f"servidor: {recv_mens}")
-            if recv_mens == 'exit':
-                print("Conexão encerrada do outro lado, escreva exit para sair")
+        try:
+            recv_mens = con_tcp.recv(1024).decode("utf8")
+            if len(recv_mens) > 1:
+                msg_list.insert(tkinter.END, recv_mens)
+                print(recv_mens)
+        except OSError:
+            break
 
 
-if __name__ == "__main__":
+def fecha():
+    """Essa funcão é chamada quando a janela é fechada"""
+    mensagem.set("exit")
+    msg = mensagem.get()
+    con_tcp.send(bytes(msg, "utf8"))
+    window.quit()
+    window.destroy()
 
-    print("Bem vindo ao Chat sem nome no momento!\n")
-    conexao = conexao()
-    chat(conexao)
 
-    try:
-        conexao.close()
-    except ConnectionError as error:
-        print("A conexao TCP esta encerrada")
+def set_ip():
+    global ip
+    ip = ips.get()
+    print(ip)
+    windowip.quit()
+    windowip.destroy()
 
-    receive_thread = Thread(target=chat)
+
+def get_ip():
+    global ip
+    return ip
+
+
+# interface grafica
+windowip = tkinter.Tk()
+windowip.title("IpBox")
+windowip.configure(bg="#ffffff")
+windowip.geometry("230x150")  # tamanho e psocionamento
+windowip.resizable(False, False)
+
+windowip.protocol("WM_DELETE_WINDOW", fecha)
+
+ips = tkinter.StringVar()  # declarando o tipo do campo mensagem
+l_digiteip = tkinter.Label(windowip, text="Digite o ip do servidor: ", font="Ubuntu 14", height=2, bg="#ffffff")
+e_mensagemip = tkinter.Entry(windowip, font="Ubuntu 12 bold", fg="#483659", width=25, textvariable=ips)
+e_mensagemip.bind("<Return>", )
+b_enviarip = tkinter.Button(windowip, text="Conectar", font="Ubuntu 14 bold", height=1, border=3,
+                            relief="groove", fg="#483659", command=set_ip)
+
+l_digiteip.grid(row=1, column=0, padx=5, pady=5)
+e_mensagemip.grid(row=20, column=0)
+b_enviarip.grid(row=25, sticky='sew', padx=5, pady=5)
+
+window = tkinter.Tk()
+window.title("ChatBox1")
+window.configure(bg="#ffffff")
+window.geometry("475x550")  # tamanho e psocionamento
+window.resizable(False, False)
+
+window.protocol("WM_DELETE_WINDOW", fecha)
+
+campo_conversa = tkinter.Frame(window, height=150)
+mensagem = tkinter.StringVar()  # declarando o tipo do campo mensagem
+scrollbar = tkinter.Scrollbar(campo_conversa)
+l_mensagem = tkinter.Label(window, text="Mensagem:", font="Ubuntu 14", width=10, height=2, bg="#ffffff")
+l_conversa = tkinter.Label(window, text=" Conversa: ", font="Ubuntu 14", height=2, bg="#ffffff")
+msg_list = tkinter.Listbox(window, height=15, width=50, font="Ubuntu 12 bold", fg="#483659", border=2,
+                           yscrollcommand=scrollbar.set)
+e_mensagem = tkinter.Entry(window, font="Ubuntu 12 bold", fg="#483659", width=50, textvariable=mensagem)
+e_mensagem.bind("<Return>", )
+b_enviar = tkinter.Button(window, text="Enviar Mensagem", font="Ubuntu 14 bold", height=1, border=3,
+                          relief="groove", fg="#483659", command=chat)
+b_sair = tkinter.Button(window, text="Exit", font="Ubuntu 14 bold", fg="red", border=3, relief='groove',
+                        command=fecha)
+
+scrollbar.grid()
+msg_list.grid(row=2, column=0)
+campo_conversa.grid(row=2, column=1)
+l_mensagem.grid(row=4, column=0, pady=5, sticky='w')
+l_conversa.grid(row=1, column=0, padx=5, pady=5)
+e_mensagem.grid(row=20, column=0)
+b_enviar.grid(row=25, sticky='w', padx=5, pady=5)
+b_sair.grid(row=25, sticky='e', pady=5)
+
+SERVER_PORT = 9050
+
+msg_list.insert(tkinter.END, "Bem vindo ao Chat sem nome no momento!\n")
+windowip.mainloop()
+ADDR = (get_ip(), SERVER_PORT)
+con_tcp = socket(AF_INET, SOCK_STREAM)
+con_tcp.connect(ADDR)
+print("conectado")
+msg_list.insert(tkinter.END, "Chat iniciado! Para sair escreva: exit ")
+msg_list.insert(tkinter.END, "Escreva seu nome: ")
+while True:
+    receive_thread = Thread(target=chatrecebe)
     receive_thread.start()
+    window.mainloop()
--- WhatszAaaaapppp/servidor.py
+++ WhatszAaaaapppp/servidor.py
@@ -1,7 +1,7 @@
-import socket
+from socket import AF_INET, socket, SOCK_STREAM, gethostbyname, gethostname
 from threading import Thread
 
-SERVER_PORT = 8001
+
 
 
 def accept_conexoes():
@@ -16,23 +16,24 @@ def trata_client(client):
     client.send(bytes(name + " está online!", "utf8"))
     msg = "%s entrou no chat!" % name
     broadcast(bytes(msg, "utf8"))
+    print(name, " - ENTROU NO SERVER")
     clients[client] = name
 
     while True:
-        # loop infinito de comunicacao
-        msg = client.recv(1024)  # recebendo uma mensagem do cliente
-        if msg != bytes("{quit}", "utf8"):
-            broadcast(msg, name + "")
-        else:   # mensagem com instrucao de saída
-            client.send(bytes("{quit}", "utf8"))
+        msg = client.recv(1024)
+        if msg != bytes("exit", "utf8"):
+            broadcast(msg, name + ": ")
+            print(name, " - mandou a seguinte msg: ", msg)
+        else:
+            client.send(bytes("exit", "utf8"))
             client.close()
             del clients[client]
             broadcast(bytes("%s saiu do chat" % name, "utf8"))
+            print(name, " - SAIU DO SERVER")
             break
 
 
 def broadcast(msg, prefix=""):
-
     for sock in clients:
         sock.send(bytes(prefix, "utf8") + msg)
 
@@ -40,76 +41,18 @@ def broadcast(msg, prefix=""):
 clients = {}
 enderecos = {}
 
-HOST = "127.0.0.1"
-
-
+SERVER_PORT = 9050
+HOST = gethostbyname(gethostname())
+print("host: ", HOST)
 ADDR = (HOST, SERVER_PORT)
 
-SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SERVER = socket(AF_INET, SOCK_STREAM)
 
 SERVER.bind(ADDR)
 
-if __name__ == "__main__":
-    SERVER.listen(1)
-    print("Aguardando conexão...")
-    ACCEPT_THREAD = Thread(target=accept_conexoes)
-    ACCEPT_THREAD.start()
-    ACCEPT_THREAD.join()
+SERVER.listen(5)
+print("Aguardando conexão...")
+ACCEPT_THREAD = Thread(target=accept_conexoes)
+ACCEPT_THREAD.start()
+ACCEPT_THREAD.join()
 SERVER.close()
-
-
-"""
-SERVER_IP = ''
-SERVER_PORT = 8000
-
-
-def bind_to_server():
-
-    con_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket_server = (SERVER_IP, SERVER_PORT)
-    con_tcp.bind(socket_server)
-    con_tcp.listen(1)
-
-    return con_tcp
-
-
-def confirma_cliente(con_tcp):
-    conexao, ip_client = con_tcp.accept()
-    print(f"\nO cliente com  o ip {ip_client[0]} se conectou!")
-    return conexao, ip_client
-
-
-def fecha_conexao(con_tcp):
-    print("Encerrando conexão e fechando o programa...")
-    con_tcp.close()
-
-
-def listening(con_tcp, ip_client):
-    print("Iniciando o chat. Aguardando mensagem...\n")
-
-    while True:
-        recv_mens = con_tcp.recv(1024).decode("ascii")
-
-        if recv_mens is not None:
-            print(f"Cliente:{ip_client[0]} :{recv_mens}")
-            if recv_mens == 'exit':
-                print("Conexão encerrada do outro lado, escreva exit para sair")
-
-        enviando_msg = input("Você: ")
-
-        if enviando_msg is not None:
-            con_tcp.send(bytes(enviando_msg ,"utf8"))
-            if enviando_msg == 'exit':
-                break
-
-    fecha_conexao(con_tcp)
-
-
-if __name__ == "__main__":
-
-    conexao_atual = bind_to_server()
-    accept_connection, cliente = confirma_cliente(conexao_atual)
-    listening(accept_connection, cliente)
-
-    sys.exit()
-"""
